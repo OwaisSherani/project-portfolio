17d3530bb851c0708d0d1f72f1556a5914efe1e6
Owais Sherani <160261768+OwaisSherani@users.noreply.github.com>
2024-11-17 14:45:52 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,14 +16,11 @@ const About = () => {
     />
     <div className="text-center lg:w-2/3 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800 hover:text-yellow-700">
-        Microdosing synth tattooed vexillologist
+       Owais Ahmed Khan Sherani
       </h1>
       <p className="mb-8 leading-relaxed text-gray-800  hover:text-yellow-700">
-        Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
-        tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt
-        ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over
-        meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af
-        fingerstache pitchfork.
+        I am graduated in BS Computer Science. I ma persuing to learn Artificial Intelligence and Mataverse 
+        in GIAIC. I have made different projects while learning.
       </p>
       <div className="flex justify-center">
         <button className="inline-flex text-5xl text-blue-700 border-0 py-2 px-6 rounded">
